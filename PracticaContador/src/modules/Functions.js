6adf125110b37e463c2f2550c173660cc2d5bbb4
Jorge Calvo <calvoblancojorge@gmail.com>
2020-03-03 09:02:33 +0100

    Miscelánea

---
+++ PracticaContador/src/modules/Functions.js
@@ -0,0 +1,28 @@
+
+// Functions
+function count(miliseconds) {
+    return new Promise(resolve => setTimeout(resolve, miliseconds))
+}
+
+export function addCounter(seconds, countersElement) {
+    let c = 0;
+
+    const counter = document.createElement('li');
+    counter.textContent = `0/${seconds}`;
+
+    const interval = setInterval(() => {
+        c++;
+        console.log(`counter: ${c}/${seconds}`)
+        counter.textContent = `${c}/${seconds}`;
+    }, 1000);
+    countersElement.appendChild(counter);
+
+    function stopCounter () {
+        clearInterval(interval);
+        counter.remove();
+    }
+
+    counter.onclick = stopCounter;
+
+    count(seconds * 1000).then(stopCounter);
+}
---
+++ PracticaContador/src/modules/main.js
@@ -0,0 +1,24 @@
+import * as Modulo1 from "./Functions.js"
+
+// Elements
+const [input, button, counters] = document.querySelectorAll('input, button, #counters');
+
+
+// Events
+input.onkeyup = (e) => {
+    const seconds = e.target.value;
+
+    button.textContent = seconds ? `Contar ${seconds} segundos` : 'Contar';
+
+    if (e.key === 'Enter') {
+        button.click();
+    }
+}
+
+button.onclick = () => {
+    if (input.value) {
+        Modulo1.addCounter(input.value, counters);
+    }
+    input.focus();
+    input.select();
+}
