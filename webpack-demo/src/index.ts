4e37a0889da2ec0430e29cb8cb7e6ace528fa9d7
Jorge Calvo <calvoblancojorge@gmail.com>
2020-03-04 12:39:25 +0100

    Primer commit

---
+++ webpack-demo/src/index.ts
@@ -0,0 +1,76 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const pending = document.querySelector('#pendingButton')
+const allButton = document.querySelector('#allButton');
+const completed = document.querySelector('#completedButton');
+
+function contar() {
+    const numTasks = document.getElementById('tasksLeft');
+    numTasks.textContent = 'Quedan ' + list.children.length + ' tareas';
+}
+
+pending.addEventListener('click', () => {
+
+    for (let i = 0; i < list.children.length; i++) {
+        console.log(i);
+        let item = list.children[i].firstChild.firstChild as HTMLInputElement;
+        if (item.checked === true) {
+            console.log("Holiiii");
+            let li = list.children[i] as HTMLInputElement;
+            li.style.display = 'none';
+        }
+    }
+});
+
+allButton.addEventListener('click', () => {
+
+    for (let i = 0; i < list.children.length; i++) {
+        console.log(i);
+        let li = list.children[i] as HTMLInputElement;
+        li.style.display = 'block';
+    }
+});
+
+completed.addEventListener('click', () => {
+
+    for (let i = 0; i < list.children.length; i++) {
+        console.log(i);
+        let item = list.children[i].firstChild.firstChild as HTMLInputElement;
+        if (item.checked === false) {
+            console.log("Holiiii");
+            let li = list.children[i] as HTMLInputElement;
+            li.style.display = 'none';
+        }
+    }
+});
+
+input.onkeyup = (e) => {
+
+    if (e.key === 'Enter' && input.value !== '') {
+
+        const listItem = document.createElement('li');
+        const divTask = document.createElement('div');
+        const check = document.createElement('input');
+        const span = document.createElement('span');
+        const icon = document.createElement('i');
+
+        check.setAttribute('type', 'checkbox');
+        span.textContent = input.value;
+        icon.innerHTML = '<i class = "material-icons">delete_outline</i>';
+
+        list.appendChild(listItem);
+        listItem.appendChild(divTask);
+        divTask.appendChild(check);
+        divTask.appendChild(span);
+        divTask.appendChild(icon);
+
+        input.value = '';
+
+        contar();
+    }
+}
+
+
+function deleteTask() {
+
+}
